5bfc9b4eda56e2701c832b76992b963e71d6a8e9
Juan Velazquez <lolgratis8@gmail.com>
2025-11-01 18:46:50 -0600

    estructura limpia

--- vetedocs/auth/index.php
+++ vetedocs/auth/index.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar sesión - Veterinario</title>
-    <link rel="stylesheet" href="../css/estilo_login.css">
+    <link rel="stylesheet" href="../css/login.css">
     
   <link rel="apple-touch-icon" type="image/jpg" href="https://i.postimg.cc/q7YcnW3s/logo.png"/>
   <link rel="shortcut icon" type="image/x-icon" href="https://i.postimg.cc/q7YcnW3s/logo.png"/>
